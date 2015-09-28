9dbe9f05a8dafd075f4558863e86666fbb56b9d1
Benjamin Balazs <benjaminbalazs@gmail.com>
2015-09-28 12:27:33 +0400

    init

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'sq-ember-normalize'
+  name: 'sq-ember-normalize',
+  included: function(app) {
+    this._super.included(app);
+    app.import('vendor/normalize.css');
+    app.import('vendor/app.css');
+  },
+  isDevelopingAddon: function() {
+    return true;
+  }
 };
