e4d0b14c98af24a3632a497382f851d3092d011f
Benjamin Balazs <benjaminbalazs@gmail.com>
2015-10-06 01:16:53 +0400

    3

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = {
     this._super.included(app);
     app.import('vendor/normalize.css');
     app.import('vendor/app.css');
+    app.import('vendor/fastclick.js');
   },
   isDevelopingAddon: function() {
     return true;
