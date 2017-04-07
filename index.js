a7af04eada250ea4172db5f48e68b317d9ba0794
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-04-07 14:48:40 +0400

    fastboot

--- index.js
+++ index.js
@@ -2,14 +2,24 @@
 'use strict';
 
 module.exports = {
+
   name: 'sq-ember-normalize',
+
   included: function(app) {
+
     this._super.included(app);
+
     app.import('vendor/normalize.css');
     app.import('vendor/app.css');
-    app.import('vendor/fastclick.js');
+
+    if ( !process.env.EMBER_CLI_FASTBOOT ) {
+        app.import('vendor/fastclick.js');
+    }
+
   },
+
   isDevelopingAddon: function() {
     return true;
   }
+
 };
