20cf8299d3fa7471ca811133919faf46bc4c3abf
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-09-28 02:26:32 +0400

    cl

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 /* jshint node: true */
 'use strict';
 
-var path = require('path');
-var map = require('broccoli-stew').map;
-var Funnel = require('broccoli-funnel');
-var MergeTrees = require('broccoli-merge-trees');
-
 module.exports = {
 
     name: 'sq-ember-normalize',
@@ -16,28 +11,6 @@ module.exports = {
 
         app.import('vendor/normalize.css');
         app.import('vendor/app.css');
-        app.import(this.treePaths.vendor + '/fastclick/fastclick.js');
-
-    },
-
-    treeForVendor: function(vendorTree) {
-
-        var trees = [];
-
-        var fastclickTree = new Funnel(path.join(path.dirname(require.resolve('fastclick'))), {
-            destDir: 'fastclick',
-            files: ['fastclick.js']
-        });
-
-        fastclickTree = map(fastclickTree, (content, relativePath) => {
-            if ( relativePath.indexOf('css') !== -1 ) { return content; }
-            return `if (typeof FastBoot === 'undefined') { ${content} }`;
-        });
-
-        if ( vendorTree !== undefined ) { trees.push(vendorTree); }
-        trees.push(fastclickTree);
-
-        return new MergeTrees(trees);
 
     },
 
